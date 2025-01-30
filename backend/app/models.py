cf65c3c4c1ba36fac6193ead234bd44c92e9f6e7
dart94 <167586944+dart94@users.noreply.github.com>
2025-01-30 08:33:52 -0700

    4.4.0 google auth

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,9 +5,10 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     is_verified = db.Column(db.Boolean, default=False, nullable=False)
+
 
 
 class PregnancyData(db.Model):
