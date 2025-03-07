cfcbff28cfa463d4a8987e6436335acd8586c01a
dart94 <167586944+dart94@users.noreply.github.com>
2025-03-06 18:55:09 -0700

    refactor: add user ID type logging in api_mi_perfil for improved debugging

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -552,6 +552,7 @@ def get_dashboard_data():
 def api_mi_perfil():
     user_id = get_jwt_identity()
     print("ID del usuario (desde el token JWT):", user_id)  # Verifica el ID del usuario
+    print("Tipo de user_id:", type(user_id), "Valor:", user_id)
 
     user = User.query.get(user_id)
     if not user:
