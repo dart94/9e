203c517fb884a34631a91eabc4c8b2dcfbbccad2
dart94 <167586944+dart94@users.noreply.github.com>
2026-03-19 18:58:24 -0700

    add test

--- mobile-app/app/(auth)/login.tsx
+++ mobile-app/app/(auth)/login.tsx
@@ -225,7 +225,7 @@ export default function LoginScreen() {
       <Text style={textStyles.title}>Iniciar Sesión</Text>
 
       <CustomInput
-        placeholder="Correo electrónico"
+        placeholder="TEST 123 🔥"
         value={email}
         onChangeText={(text) => {
           setEmailError(false);
